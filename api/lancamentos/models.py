d1dbbf9b36d63e79cd2c9b2e45bf987e82f42e19
Luan C. Redmann <luandacostaredmann@gmail.com>
2022-08-15 14:20:12 -0300

    ✨ modelo de lançamento iniciado

--- api/lancamentos/models.py
+++ api/lancamentos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Lancamento(models.Model):
+    titulo = models.CharField(max_length = 20, help_text = 'É uma breve descrição do lançamento.')
+    entrada = models.BooleanField()
+    descricao = models.TextField(max_length = 45, help_text = 'É uma descrição detalhada do lançamento.')
+    valor = models.DecimalField(decimal_places = 2, max_digits = 10)
+
+    def __str__(self):
+        return ("+" if self.entrada else "-")
