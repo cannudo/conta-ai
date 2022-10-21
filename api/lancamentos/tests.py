21aff598632d4cf19b3d9361958ece17fd19c6b9
Luan C. Redmann <luandacostaredmann@gmail.com>
2022-10-21 14:52:43 -0300

    🧪 começa a escrever os testes

--- api/lancamentos/tests.py
+++ api/lancamentos/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Lancamento
+from fluxodecaixa.models import FluxoDeCaixa
+from datetime import date
 
-# Create your tests here.
+
+class LancamentoTestCase(TestCase):
+    def setUp(self):
+        f = FluxoDeCaixa.objects.create(data = date.today())
+        l = Lancamento.objects.create(titulo = "Pix da Mary", entrada = True, descricao = "Mesada", valor = 250, fluxodecaixa = f)
+
+    def test(self):
+        test = Lancamento.objects.first()
+        print(test)
