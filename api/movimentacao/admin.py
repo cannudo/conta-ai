a0b80f5175612e6695a5d80f5b9e9ad8c60868c0
Luan C. Redmann <luandacostaredmann@gmail.com>
2022-11-29 11:16:53 -0300

    💥 modelagem de movimentação

--- api/movimentacao/admin.py
+++ api/movimentacao/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Movimentacao
 # Register your models here.
+
+admin.site.register(Movimentacao)
--- api/movimentacao/models.py
+++ api/movimentacao/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movimentacao(models.Model):
+    titulo = models.CharField(max_length = 35)
+    descricao = models.TextField(max_length = 70, null = True)
+    entrada = models.BooleanField(default = True)
+    valor = models.FloatField()
+    data = models.DateField()
+
+    def __str__(self):
+        return f'R$ {self.valor} — {self.titulo} [{self.data.day}/{self.data.month}/{self.data.year}]'
