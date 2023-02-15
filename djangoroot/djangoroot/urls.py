afbf0dd4460f3ce7e1598c7f838a91caebaba3e5
Luan C. Redmann <luandacostaredmann@gmail.com>
2023-02-15 19:25:45 -0300

    👔 expõe movimentação em API

--- djangoroot/djangoroot/urls.py
+++ djangoroot/djangoroot/urls.py
@@ -1,23 +1,8 @@
-"""api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from movimentacao.viewsets import MovimentacaoViewSet
+from movimentacao.api.viewsets import MovimentacaoViewSet
 
 router = routers.DefaultRouter()
 
@@ -25,6 +10,5 @@ router.register(r'movimentacao', MovimentacaoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls))
 ]
--- djangoroot/movimentacao/serializers.py
+++
@@ -1,7 +0,0 @@
-from movimentacao.models import Movimentacao
-from rest_framework import serializers
-
-class MovimentacaoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movimentacao
-        fields = ['titulo', 'descricao', 'entrada', 'valor', 'data']
