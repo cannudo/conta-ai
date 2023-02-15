9d60d6f8c039902aa0a9a4222cd0cb8b1fe27bc5
Luan C. Redmann <luandacostaredmann@gmail.com>
2023-02-15 17:23:26 -0300

    🧪 melhora o model de movimentação

--- djangoroot/movimentacao/models.py
+++ djangoroot/movimentacao/models.py
@@ -3,10 +3,23 @@ from django.db import models
 # Create your models here.
 class Movimentacao(models.Model):
     titulo = models.CharField(max_length = 35)
-    descricao = models.TextField(max_length = 70, null = True)
+    descricao = models.TextField(max_length = 140, null = True)
     entrada = models.BooleanField(default = True)
     valor = models.FloatField()
     data = models.DateField()
 
     def __str__(self):
-        return f'R$ {self.valor} — {self.titulo} [{self.data.day}/{self.data.month}/{self.data.year}]'
+        '''
+            Essa não é a maneira mais funcional
+            de se passar uma função no retorno
+            de outra.
+
+            Mas eu tentei.
+
+
+            Alô @lrlucena !
+        '''
+        sinal = "↑" if (self.entrada) else "↓"
+        output = sinal + f' R$ {self.valor} — {self.titulo} [{self.data.day}/{self.data.month}/{self.data.year}]'
+
+        return output
